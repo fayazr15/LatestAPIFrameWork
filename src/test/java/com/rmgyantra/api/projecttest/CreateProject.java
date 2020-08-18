3d7fec7ba3882d3032f575cff5cc0fc251d2b7f0
“MECHANICAL <baig20202@gmail.com>
2020-08-18 20:49:18 +0530

    Latest API FrameWork By Deepak Sir

---
+++ src/test/java/com/rmgyantra/api/projecttest/CreateProject.java
@@ -0,0 +1,65 @@
+package com.rmgyantra.api.projecttest;
+
+import static io.restassured.RestAssured.given;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.rmgyantra.api.pojoClassLib.Project;
+import com.rmgyantra.genericlib.BaseClass;
+import com.rmgyantra.genericlib.DataBaseUtilities;
+import com.rmgyantra.genericlib.IEndPoints;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+
+public class CreateProject extends BaseClass{
+	
+	
+
+	@Test
+	public void createProjectTest() throws Throwable {
+		
+		String actAPIProjectNAme = "ABC";
+		String projectStatus = "Completed";
+        Project pObj = new Project(actAPIProjectNAme, "aug", "deepak", projectStatus, 12);
+		
+   	 
+        Response resp =	given()
+                        .contentType(ContentType.JSON)
+                        .body(pObj)
+                        .when()
+                        .post(IEndPoints.addSinglePRoject);
+			   		resp.then()
+				       	.assertThat().statusCode(201)
+				       	.and()
+				       	.assertThat().contentType(ContentType.JSON)
+				       	.and()
+				        .log().all();
+			   		
+			   		
+       String scuMg = resp.jsonPath().get("msg");
+       Assert.assertEquals(scuMg, "Successfully Added");
+       
+              //Connect to dataDase
+       String dbProjectNAme = DataBaseUtilities.executeQueryAndGetData("select *from project", 4, actAPIProjectNAme);
+       Assert.assertEquals(dbProjectNAme, actAPIProjectNAme);
+       
+	
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
